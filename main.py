fc3ba87b20c408d63c3bcadfaa5985187ce3b3a1
nbril0313 <mseokn@gmail.com>
2024-02-17 16:42:56 +0900

    committed files

--- main.py
+++ main.py
@@ -84,4 +84,128 @@ class DANN(object):
 
         if do_random_init:
             W = self.random_init(nb_features, self.hidden_layer_size)
-            V =
+            V = self.random_init(self.hidden_layer_size, nb_labels)
+            b = np.zeros(self.hidden_layer_size)
+            c = np.zeros(nb_labels)
+            U = np.zeros(self.hidden_layer_size)
+            d = 0.
+        else:
+            W, V, b, c, U, d = self.W, self.V, self.b, self.c, self.U, self.d
+
+        best_valid_risk = 2.0
+        continue_until = 30
+
+        for t in range(self.maxiter):
+            for i in range(nb_examples):
+                x_t, y_t = X[i,:], Y[i]
+
+                hidden_layer = self.sigmoid(np.dot(W, x_t) + b)
+                output_layer = self.softmax(np.dot(V, hidden_layer) + c)
+
+                y_hot = np.zeros(nb_labels)
+                y_hot[y_t] = 1.0
+
+                delta_c = output_layer - y_hot
+                delta_V = np.dot(delta_c.reshape(-1,1), hidden_layer.reshape(1,-1))
+                delta_b = np.dot(V.T, delta_c) * hidden_layer * (1.-hidden_layer)
+                delta_W = np.dot(delta_b.reshape(-1,1), x_t.reshape(1,-1))
+
+                if self.lambda_adapt == 0.:
+                    delta_U, delta_d = 0., 0.
+                else:
+                    # add domain adaptation regularizer from current domain
+                    gho_x_t = self.sigmoid(np.dot(U.T, hidden_layer) + d)
+
+                    delta_d = self.lambda_adapt * (1. - gho_x_t)
+                    delta_U = delta_d * hidden_layer
+
+                    if self.adversarial_representation:
+                        tmp = delta_d * U * hidden_layer * (1. - hidden_layer)
+                        delta_b += tmp
+                        delta_W += tmp.reshape(-1,1) * x_t.reshape(1,-1)
+
+                    # add domain adaptation regularizer from other domain
+                    i_2 = np.random.randint(nb_examples_adapt)
+                    x_t_2 = X_adapt[i_2, :]
+                    hidden_layer_2 = self.sigmoid(np.dot(W, x_t_2) + b)
+                    gho_x_t_2 = self.sigmoid(np.dot(U.T, hidden_layer_2) + d)
+
+                    delta_d -= self.lambda_adapt * gho_x_t_2
+                    delta_U -= self.lambda_adapt * gho_x_t_2 * hidden_layer_2
+
+                    if self.adversarial_representation:
+                        tmp = -self.lambda_adapt * gho_x_t_2 * U * hidden_layer_2 * (1. - hidden_layer_2)
+                        delta_b += tmp
+                        delta_W += tmp.reshape(-1,1) * x_t_2.reshape(1,-1)
+
+                W -= delta_W * self.learning_rate
+                b -= delta_b * self.learning_rate
+
+                V -= delta_V * self.learning_rate
+                c -= delta_c * self.learning_rate
+
+                U += delta_U * self.learning_rate
+                d += delta_d * self.learning_rate
+            # END for i in range(nb_examples)
+
+            self.W, self.V, self.b, self.c, self.U, self.d = W, V, b, c, U, d
+
+            # early stopping
+            if X_valid is not None:
+                valid_pred = self.predict(X_valid)
+                valid_risk = np.mean(valid_pred != Y_valid)
+                if valid_risk <= best_valid_risk:
+                    if self.verbose:
+                        print('[DANN best valid risk so far] %f (iter %d)' % (valid_risk, t))
+                    best_valid_risk = valid_risk
+                    best_weights = (W.copy(), V.copy(), b.copy(), c.copy())
+                    best_t = t
+                    continue_until = max(continue_until, int(1.5*t))
+                elif t > continue_until:
+                    if self.verbose:
+                        print('[DANN early stop] iter %d' %t)
+                    break
+        # END for t in range(self.maxiter)
+
+        if X_valid is not None:
+            self.W, self.V, self.b, self.c = best_weights
+            self.nb_iter = best_t
+            self.valid_risk = best_valid_risk
+        else:
+            self.nb_iter = self.maxiter
+            self.valid_risk = 2.
+
+    def forward(self, X):
+        """
+         Compute and return the network outputs for X, i.e., a 2D array of size len(X) by len(set(Y)).
+         the ith row of the array contains output probabilities for each class for the ith example.
+
+        """
+        hidden_layer = self.sigmoid(np.dot(self.W, X.T) + self.b[:,np.newaxis])
+        output_layer = self.sigmoid(np.dot(self.V, hidden_layer) + self.c[:,np.newaxis])
+        return output_layer
+
+    def hidden_representation(self, X):
+        """
+         Compute and return the network hidden layer values for X.
+        """
+        hidden_layer = self.sigmoid(np.dot(self.W, X.T) + self.b[:,np.newaxis])
+        return hidden_layer.T
+
+    def predict(self, X):
+        """
+         Compute and return the label predictions for X, i.e., a 1D array of size len(X).
+         the ith row of the array contains the predicted class for the ith example .
+        """
+        output_layer = self.forward(X)
+        return np.argmax(output_layer, 0)
+
+    def preduct_domain(self, X):
+        """
+         Compute and return the domain predictions for X, i.e., a 1D array of size len(X).
+         the ith row of the array contains the predicted domain (0 or 1) for the ith example.
+        """
+        hidden_layer = self.sigmoid(np.dot(self.W, X.T) + self.b[:, np.newaxis])
+        output_layer = self.sigmoid(np.dot(self.U, hidden_layer) + self.d)
+        return np.array(output_layer < .5, dtype=int)
+
